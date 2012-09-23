bbeef4d3c11190f69d276fad1c8998f5b1aed724
Garrick S. Bodine <garrick.bodine@gmail.com>
2012-09-23 09:51:15 -0400

    Moved plugin footer hook below the other content

--- common/footer.php
+++ common/footer.php
@@ -1,23 +1,7 @@
         </div><!-- end content -->
 
         <div id="footer" class="container">
-            <div class="row">
-                <div class="span12">
-                    <hr />
-                    <?php plugin_footer(); ?>
-                </div>
-            </div>
-         <?php 
-            // killing the nave in the footer -- not sure why anyone would want it...
-            /* <div class="row">
-                <div class="span12">
-                <ul class="navigation navbar">
-                    <?php echo public_nav_main(array(__('Home') => uri(''), __('Browse Items') => uri('items'), __('Browse Collections') => uri('collections'))); ?>
-                </ul>
-                </div>
-            </div>
-            */ ?>
-            
+            <div class="row"><div class="span12"><hr /></div></div>
             <div id="footer-text" class="row">
                 <div class="span8"> 
                     <?php echo html_entity_decode(get_theme_option('Footer Text')); ?>
@@ -28,11 +12,17 @@
                 </div>
                 <div class="span4">
                     <div class="pull-right">
-                       
-                        <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
+                        <p><small><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></small></p>
+                        <?php  ?>
                     </div>
                 </div>
             </div>
+            <div class="row">
+                <div class="span12">
+                    <?php plugin_footer(); ?>
+                </div>
+            </div>
+            
         </div><!-- end footer -->
     </div><!-- end wrap -->
 </body>
