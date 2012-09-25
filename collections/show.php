c23cc38437e5f8d2a0f8f714f4efa2eeb04e8d62
Garrick S. Bodine <garrick.bodine@gmail.com>
2012-09-25 10:09:06 -0400

    Fixed individual collection layout. Closes #11.

--- collections/show.php
+++ collections/show.php
@@ -1,31 +1,62 @@
-<?php head(array('title' => html_escape($collection->name),'bodyid'=>'collections','bodyclass' => 'show')); ?>
+<?php 
+    head(array('title' => html_escape($collection->name),
+            'bodyid'=>'collections',
+            'bodyclass' => 'show')
+    ); 
+
+?>
 
 <div id="primary" class="show">
     
-    <div id="collection-title">
-
-	    <h1><?php echo collection('Name'); ?></h1>
-    
+    <div class="row" id="collection-title">
+        <div class="span12">
+            <h1 class="page-header"><?php echo collection('Name'); ?><br /><small><?php echo total_items_in_collection(); ?> item<?php if (total_items_in_collection() != 1)  echo 's';  ?> in collection</small></h1>
+        </div>
     </div>
-
-    <p class="view-items-link-browse"><?php echo link_to_browse_items('View all items', array('collection' => collection('id'))); ?></p>
-
-    <div id="collection-description" class="element">
-        <h2>Description</h2>
-        <div class="element-text"><?php echo nls2p(collection('Description')); ?></div>
-        <?php if (collection_has_collectors()): ?>
+    <div class="row">
+        <div id="collection-description" class="span8">
+            <div class="lead"><?php echo nls2p(collection('Description')); ?></div>
+        </div>
+        <div class="span4">
+            <?php if (collection_has_collectors()): ?>
         	<div class="element">
-                <h3>Collector(s)</h3>
+                <h4>Collector(s)</h4>
             	    <div class="element-text">
-                    <p><?php echo collection('Collectors', array('delimiter'=>', ')); ?></p>
-			</div>
-        </div>            	   
-		<?php endif; ?>
+                        <p><?php echo collection('Collectors', array('delimiter'=>', ')); ?></p>
+                    </div>
+            <?php endif; ?>
+         </div>
+         </div>
+    </div>
+    <div class="row">
+        <div class="span12">
+            <hr />
+        </div>
+    </div>
+    <div class="row">
+        <?php while (loop_items_in_collection(4)): ?>
+        <div class="span3">
+            <div class="well" style="text-align:center;">
+                <div><?php echo link_to_item(item_square_thumbnail($props=array('class'=>'img img-rounded img-polaroid'))); ?></div>
+                <br />
+                <p><small><strong><?php echo item('Dublin Core','Title'); ?></strong></small></p>
+            </div>
+        </div>
+        <?php endwhile ?>
+    </div>
+    <div class="row">
+        <div class="span12">
+            <p class="view-items-link-browse lead" style="text-align:center"><?php echo link_to_browse_items('Browse all items in the collection', array('collection' => collection('id'))); ?></p>
+        
+        </div>
     </div>
     <!-- end collection-description -->
+    <div class="row">
+        <div class="span12">
+            <?php echo plugin_append_to_collections_show(); ?>
+        </div>
+    </div>
     
-    <?php echo plugin_append_to_collections_show(); ?>
-
 </div><!-- end primary -->
 
 <?php foot(); ?>
