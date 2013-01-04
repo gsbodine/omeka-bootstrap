00184ca9117d1d519d7c6c53ccb00c1a6e0726ca
Garrick S. Bodine <garrick.bodine@gmail.com>
2013-01-03 21:31:27 -0500

    Update some of the base items pages to be more 2.0 compatible.

--- items/tags.php
+++ items/tags.php
@@ -5,11 +5,8 @@ echo head(array('title'=>$pageTitle,'bodyid'=>'items','bodyclass'=>'tags'));
 
 <div id="primary">
 
-    <div class="page-title"><h1><?php echo $pageTitle; ?> <small>By Current Tags</small></h1></div>
-
-    <ul class="navigation item-tags nav nav-pills" id="secondary-nav">
-        <?php echo public_nav_items(); ?>
-    </ul>
+    <div class="page-title"><h1><i class="icon-tags"></i> <?php echo $pageTitle; ?> <small>By Current Tags </small></h1></div>
+    <?php echo public_nav_items()->setUlClass('nav nav-pills'); ?>
     
     <?php 
         asort($tags);
--- js/site.js
+++ js/site.js
@@ -15,10 +15,10 @@ jQuery(document).ready(function($){
     
     // making tags look like labels and adding the icons
     $('a[rel="tag"]').addClass("label label-inverse").prepend('<i class="icon-tag icon-white"></i> ');
-    $('.popular,').addClass("btn btn-mini").prepend('<i class="icon-tag"></i> ');
-    $('.v-popular,.vv-popular,.vvv-popular').addClass("btn btn-small").prepend('<i class="icon-tag"></i> ');
-    $('.vvvv-popular,.vvvvv-popular,.vvvvvv-popular').addClass("btn").prepend('<i class="icon-tag"></i> ');
-    $('.vvvvvvv-popular,.vvvvvvvv-popular').addClass("btn btn-large").prepend('<i class="icon-tag"></i> ');
+    $(".popular").addClass("btn btn-mini").prepend('<i class="icon-tag"></i> ');
+    $('.v-popular,.vv-popular,.vvv-popular').addClass("btn btn-small btn-info").prepend('<i class="icon-tag"></i> ');
+    $('.vvvv-popular,.vvvvv-popular,.vvvvvv-popular').addClass("btn btn-primary").prepend('<i class="icon-tag"></i> ');
+    $('.vvvvvvv-popular,.vvvvvvvv-popular').addClass("btn btn-large btn-success").prepend('<i class="icon-tag"></i> ');
     
     $('.dropdown-toggle').dropdown();
     
