de0a9a1dde631d48898f10b380f2a2dafcdb793a
Garrick S. Bodine <garrick.bodine@gmail.com>
2013-01-16 21:22:15 -0500

    Prepping for exhibit theming, though it doesn't seem to work currently :(

---
+++ exhibits/tags.php
@@ -0,0 +1,21 @@
+<?php
+$title = __('Browse Exhibits by Tag');
+echo head(array('title' => $title, 'bodyid' => 'exhibit', 'bodyclass' => 'tags'));
+?>
+<h1><?php echo $title; ?></h1>
+<ul class="navigation exhibit-tags" id="secondary-nav">
+    <?php echo nav(array(
+            array(
+                'label' => __('Browse All'),
+                'uri' => url('exhibits/browse')
+            ),
+            array(
+                'label' => __('Browse by Tag'),
+                'uri' => url('exhibits/tags')
+            )
+        )
+    ); ?>
+</ul>
+
+<?php echo tag_cloud($tags, 'exhibits/browse'); ?>
+<?php echo foot(); ?>
