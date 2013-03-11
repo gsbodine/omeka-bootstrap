39aba777efa25f4c13d7a6f09a83123c23108fb0
Garrick S. Bodine <garrick.bodine@gmail.com>
2013-03-10 22:58:43 -0400

    Updates for search form styling for Omeka 2.0. References issue #10.

--- items/search.php
+++ items/search.php
@@ -4,16 +4,18 @@ echo head(array('title' => $pageTitle,
            'bodyclass' => 'items advanced-search',
            'bodyid' => 'items'));
 ?>
-<div id="primary">
-<h1><i class="icon-search"></i> <?php echo $pageTitle; ?></h1>
 
-    <nav class="items-nav navigation" id="secondary-nav">
-        <?php echo public_nav_items()->setUlClass('nav nav-pills'); ?>
-    </nav>
-
-<?php echo $this->partial('items/search-form.php',
-    array('formAttributes' =>
-        array('id'=>'advanced-search-form'))); ?>
-
+<div id="primary" class="row">
+    <div class="span12">
+        <h1><i class="icon-search"></i> <?php echo $pageTitle; ?></h1>
+        <nav class="items-nav navigation" id="secondary-nav">
+            <?php echo public_nav_items()->setUlClass('nav nav-pills'); ?>
+        </nav>
+        <hr />
+        <?php echo $this->partial('items/search-form.php',
+            array('formAttributes' =>
+            array('id'=>'advanced-search-form'))); 
+        ?>
+    </div>
 </div> <!-- Close 'primary' div. -->
 <?php echo foot();
