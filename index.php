bc2e7f795bf1b7f46e342f26aa9a9d8faf6daae2
Amanda Regan <regan008@cougars.csusm.edu>
2015-07-12 15:15:04 -0400

    fixing some styling on homepage featured exhibit. Added map to the homepage

--- index.php
+++ index.php
@@ -55,50 +55,12 @@
     </div>
     <div class="col-md-4 home-map">
     <h2>Map</h2>
+    <img src='http://localhost:8888/wgh/themes/WearingGayHistoryTheme/images/mapimg.jpg' style="width:325px;"/>
     </div>
 </div>
 
-<div class="row home-features"> <!-- start featured exhibit and recent items -->
-    <div class="col-md-8"> <!--feat exhibit-->
-        <?php if ((get_theme_option('Display Featured Exhibit') !== '0')
-            && plugin_is_active('ExhibitBuilder')
-            && function_exists('exhibit_builder_display_random_featured_exhibit')): ?>
-        <!-- Featured Exhibit -->
-            <?php echo exhibit_builder_display_random_featured_exhibit(); ?>
-        <?php endif; ?>    
-    </div><!-- end feat exhibit-->
-    
-    <div class="col-md-4"> <!--recent items -->
 
-<div id="recent-items">
-            <h2><?php echo __('Recently Added Items'); ?></h2>
-            <?php
-                $homepageRecentItems = (int)get_theme_option('Homepage Recent Items') ? get_theme_option('Homepage Recent Items') : '3';
-                set_loop_records('items', get_recent_items($homepageRecentItems));
-                if (has_loop_records('items')):
-            ?>
-            <div class="items-list">
-                <?php foreach (loop('items') as $item): ?>
-                <div class="item">
-                    <h3><?php echo link_to_item(); ?></h3>
-                    <?php if (metadata('item', 'has thumbnail')): ?>
-                    <div class="item-img">
-                        <?php echo link_to_item(item_image('square_thumbnail', array('class' => 'img-responsive'))); ?>
-                    </div>
-                    <?php endif; ?>
-                    <?php if($desc = metadata('item', array('Dublin Core', 'Description'), array('snippet' => 150))): ?>
-                    <div class="item-description"><?php echo $desc; ?><?php echo link_to_item('see more', (array('class' => 'show'))); ?></div>
-                    <?php endif; ?>
-                </div>
-                <?php endforeach; ?>
-            </div>
-            <?php else: ?>
-            <p><?php echo __('No recent items available.'); ?></p>
-            <?php endif; ?>
-            <p class="view-items-link"><a href="<?php echo html_escape(url('items')); ?>"><?php echo __('View All Items'); ?></a></p>
-        </div><!--end recent-items -->
-    </div>
-</div>
+
 
 
 
