c6f68e7eec18cd43368aed10f756de3318104e9a
Amanda Regan <Amanda@Amandas-iMac.local>
2015-07-16 08:50:24 -0400

    reverted tags back to original code from forked branch. Not ideal because the code is trying to use icons that aren't included in theme but will work for now until I figure out a fix.

--- javascripts/globals.js
+++ javascripts/globals.js
@@ -69,11 +69,11 @@ jQuery(document).ready(function($){
 //    $('.carousel').carousel();
 
     // making tags look like labels and adding the icons
-    $('a[rel="tag"]').addClass("label label-primary");
-    $(".popular").addClass("btn btn-default");
-    $('.v-popular,.vv-popular,.vvv-popular');
-    $('.vvvv-popular,.vvvvv-popular,.vvvvvv-popular').addClass("btn btn-primary");
-    $('.vvvvvvv-popular,.vvvvvvvv-popular').addClass("btn btn-large btn-success");
+    $('a[rel="tag"]').addClass("label label-primary").prepend('<i class="icon-tag icon-white"></i> ');
+    $(".popular").addClass("btn btn-default").prepend('<i class="icon-tag"></i> ');
+    $('.v-popular,.vv-popular,.vvv-popular').addClass("btn btn-small btn-info").prepend('<i class="icon-tag"></i> ');
+    $('.vvvv-popular,.vvvvv-popular,.vvvvvv-popular').addClass("btn btn-primary").prepend('<i class="icon-tag"></i> ');
+    $('.vvvvvvv-popular,.vvvvvvvv-popular').addClass("btn btn-large btn-success").prepend('<i class="icon-tag"></i> ');
 
 //    $('.dropdown-toggle').dropdown();
 
