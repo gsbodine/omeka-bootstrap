304083fafca824d2c9584051454ce846e7b3ca35
Daniel Berthereau <Daniel.github@Berthereau.net>
2016-12-19 00:00:00 +0100

    Genericized simple contact form.

---
+++ simple-contact-form/index/index.php
@@ -0,0 +1,66 @@
+<?php
+$pageTitle = __('Contact Us');
+echo head(array(
+    'title' => $pageTitle,
+    'bodyclass' => 'contact-us',
+    'bodyid' => 'contact-us',
+));
+?>
+<div id="primary">
+    <?php echo flash(); ?>
+    <h1><?php echo html_escape(get_option('simple_contact_page_contact_title')); ?></h1>
+    <div id="form-instructions">
+        <?php echo get_option('simple_contact_page_contact_text'); // HTML ?>
+    </div>
+<div id="simple-contact">
+<?php
+// Keep the form of the fork of Simple Contact.
+$options = array(
+    'form_attributes' => array('class' => 'form-horizontal'),
+    'name' => $name,
+    'email' => $email,
+    'message' => $message,
+    'captcha' => $captcha,
+    'path' => 'contact',
+);
+echo $this->form('contact_form', $options['form_attributes']); ?>
+    <div class="field form-group">
+        <?php echo $this->formLabel('name', __('Your Name'), array('class' => 'control-label sr-only')); ?>
+        <div class="col-xs-12">
+            <div class="input-group">
+                <span class="input-group-addon"><span class="glyphicon glyphicon-user"></span></span>
+                <?php echo $this->formText('name', $options['name'], array('class' => 'form-control', 'placeholder' => __('Your Name'))); ?>
+            </div>
+        </div>
+    </div>
+    <div class="field form-group">
+        <?php echo $this->formLabel('email', __('Your Email'), array('class' => 'control-label sr-only')); ?>
+        <div class="col-xs-12">
+            <div class="input-group">
+                <span class="input-group-addon">@</span>
+                <?php echo $this->formText('email', $options['email'], array('class' => 'form-control', 'placeholder' => __('Your Email'))); ?>
+            </div>
+        </div>
+    </div>
+    <div class="field form-group">
+        <?php echo $this->formLabel('message', __('Your Message'), array('class' => 'control-label sr-only')); ?>
+        <div class="col-xs-12">
+            <?php echo $this->formTextarea('message', $options['message'], array('class' => 'form-control', 'rows' => '10', 'placeholder' => __('Your message'))); ?>
+        </div>
+    </div>
+
+    <?php if ($options['captcha']): ?>
+    <div class="field form-group">
+        <?php echo $options['captcha']; ?>
+    </div>
+    <?php endif; ?>
+    <?php echo $this->formHidden('path', $options['path']); ?>
+    <div class="form-group">
+        <div class="col-xs-12">
+            <?php echo $this->formSubmit('send', __('Send Message'), array('class' => 'btn btn-primary')); ?>
+        </div>
+    </div>
+</form>
+</div>
+</div>
+<?php echo foot();
---
+++ simple-contact/index/write.php
@@ -0,0 +1,17 @@
+<?php
+$pageTitle = __('Contact Us');
+echo head(array(
+    'title' => $pageTitle,
+    'bodyclass' => 'contact-us',
+    'bodyid' => 'contact-us',
+));
+?>
+<div id="primary">
+    <?php echo flash(); ?>
+    <h1><?php echo html_escape(get_option('simple_contact_page_contact_title')); ?></h1>
+    <div id="form-instructions">
+        <?php echo get_option('simple_contact_page_contact_text'); // HTML ?>
+    </div>
+    <?php echo $this->simpleContactForm(array('form_attributes' => array('class' => 'form-horizontal'))); ?>
+</div>
+<?php echo foot();
