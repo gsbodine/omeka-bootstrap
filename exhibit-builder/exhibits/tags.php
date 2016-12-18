c727652e266d5c84a90c61dc286b082442614bc1
Daniel Berthereau <Daniel.github@Berthereau.net>
2016-12-19 00:00:00 +0100

    Integrated Exhibits pages.

--- exhibit-builder/exhibits/tags.php
+++ exhibit-builder/exhibits/tags.php
@@ -5,18 +5,24 @@ echo head(array(
     'bodyclass' => 'exhibits tags',
 ));
 ?>
-<h1><?php echo $title; ?></h1>
-<ul class="navigation exhibit-tags" id="secondary-nav">
+<div id="primary">
+    <div id="exhibits-title" class="row page-header">
+        <div class="col-xs-12">
+            <h1><?php echo $title; ?></h1>
+        </div>
+    </div>
+<nav class="navigation exhibits-tags" id="secondary-nav">
     <?php echo nav(array(
-        array(
-            'label' => __('Browse All'),
-            'uri' => url('exhibits/browse'),
-        ),
-        array(
-            'label' => __('Browse by Tag'),
-            'uri' => url('exhibits/tags'),
-        ),
-    )); ?>
-</ul>
+            array(
+                'label' => __('Browse All'),
+                'uri' => url('exhibits/browse'),
+            ),
+            array(
+                'label' => __('Browse by Tag'),
+                'uri' => url('exhibits/tags'),
+            )
+    ))->setUlClass('nav nav-pills'); ?>
+</nav>
+
 <?php echo tag_cloud($tags, 'exhibits/browse'); ?>
-<?php echo foot(); ?>
+<?php echo foot();
