27b7a57706517d6833553966cc342c363a6f65f0
Daniel Berthereau <Daniel.github@Berthereau.net>
2016-12-19 00:00:00 +0100

    Simplified collections/browse.

--- collections/browse.php
+++ collections/browse.php
@@ -14,47 +14,42 @@ echo head(array(
     </div>
     <?php endif ?>
     <div class="row">
-        <div class="col-sm-12 col-md-12">
+    <?php foreach(loop('collections') as $collection): ?>
+        <div class="col-sm-6 col-md-push-0">
             <div class="row">
-            <?php foreach(loop('collections') as $collection): ?>
-                <div class="col-sm-6 col-md-6">
-                    <div class="row">
-                        <div class="col-sm-6 col-md-6"><h2><?php echo link_to_collection(); ?></h2></div>
-                        <div class="col-sm-4 col-md-4">
-                            <div class="element">
-                                <div class="collection-description">
-                                    <?php echo text_to_paragraphs(metadata('collection', array('Dublin Core', 'Description'), array('snippet' => 150))); ?>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-2 col-md-2">
-                            <?php if (metadata('collection',array('Dublin Core', 'Contributor'))): ?>
-                            <div class="element well well-sm">
-                                <p><span class="glyphicon glyphicon-user"></span> <strong><?php echo __('Collector(s)'); ?></strong></p>
-                                <div class="element-text">
-                                    <p><?php echo metadata('collection',array('Dublin Core', 'Contributor')); ?></p>
-                                </div>
-                            </div>
-                            <?php endif; ?>
-                        </div>
+                <h2><?php echo link_to_collection(); ?></h2>
+                <div class="element">
+                    <div class="collection-description">
+                        <?php echo text_to_paragraphs(metadata('collection', array('Dublin Core', 'Description'), array('snippet' => 150))); ?>
                     </div>
-                    <div class="row">
-                        <div class="col-sm-6 col-md-6">
-                            <?php fire_plugin_hook('public_collections_browse_each', array('view' => $this, 'collection' => $collection)); ?>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-sm-6 col-md-6">
-                            <p class="view-items-link-browse"><?php echo link_to_items_in_collection($text = 'View the items in this collection'); ?></p>
+                </div>
+            </div>
+            <?php if (metadata('collection',array('Dublin Core', 'Contributor'))): ?>
+            <div class="row">
+                <div class="col-xs-offset-1 col-xs-10">
+                    <div class="element well well-sm well-md well-lg">
+                        <p><span class="glyphicon glyphicon-user"></span> <strong><?php echo __('Collector(s)'); ?></strong></p>
+                        <div class="element-text">
+                            <p><?php echo metadata('collection',array('Dublin Core', 'Contributor')); ?></p>
                         </div>
                     </div>
                 </div>
-            <!-- end class="collection" -->
-            <?php endforeach; ?>
+            </div>
+            <?php endif; ?>
+            <div class="row">
+                <?php fire_plugin_hook('public_collections_browse_each', array('view' => $this, 'collection' => $collection)); ?>
+            </div>
+            <div class="row">
+                <div class="col-xs-offset-1 col-xs-10">
+	                <p class="view-items-link-browse pull-right"><?php echo link_to_items_in_collection($text = 'View the items in this collection'); ?></p>
+                </div>
+            </div>
         </div>
-        <div class="row">
-            <?php fire_plugin_hook('public_collections_browse', array('collections' => $collections, 'view' => $this)); ?>
-        </div>
+    <!-- end class="collection" -->
+    <?php endforeach; ?>
     </div>
-</div><!-- end primary -->
-<?php echo foot(); ?>
+    <div class="row">
+        <?php fire_plugin_hook('public_collections_browse', array('collections' => $collections, 'view' => $this)); ?>
+    </div>
+</div><?php // end primary ?>
+<?php echo foot();
